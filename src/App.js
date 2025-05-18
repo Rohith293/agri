6eafddb6c870294d3eb8f9083fe3072a68f6adb9
Rohith <149501424+Rohith293@users.noreply.github.com>
2025-05-18 12:28:44 +0530

    Update App.js
    updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import Login from "./components/Auth/Login.js";
 import Signup from "./components/Auth/Signup.js";
 import Dashboard from "./components/Dashboard/Dashboard.js";
@@ -10,6 +10,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
